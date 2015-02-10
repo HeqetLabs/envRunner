b592abff2167ed8b17523e27a285459345bce442
Sean Reed <sean@cheapfit.me>
2015-02-09 21:09:02 -0500

    Remote config loading

--- envRunner.py
+++ envRunner.py
@@ -33,6 +33,14 @@ def _read_yaml_file(filename):
 def _read_json_file(filename):
   return json.load(open(filename, "r"))
 
+def read_remote_file(url):
+  r = requests.get(url)
+
+  try:
+    return json.loads(r.text)
+  except (ValueError):
+    return yaml.load(r.text)
+
 if __name__ == '__main__':
   arguments = docopt(__doc__, version='envRunner v0.1')
   parsed_uri = urlparse(arguments["<config>"])
@@ -40,14 +48,11 @@ if __name__ == '__main__':
   environment = {}
   if "http" in parsed_uri.scheme:
     #read from web
-    pass
+    environment = read_remote_file(parsed_uri.geturl())
+    print environment
   elif "file" in parsed_uri.scheme or parsed_uri.scheme == "":
-   environment = read_local_file(parsed_uri.path) 
+    environment = read_local_file(parsed_uri.path) 
 
-  for k, v in environment.items():
-    print k, "=", v
-
-  env = dict(os.environ, **environment) 
-  process = subprocess.Popen(arguments["COMMAND"], env=env)
-
-  process.wait()
+  env = dict(os.environ, **{ k: str(environment[k]) for k in environment})
+  if arguments["exec"]:
+    process = subprocess.Popen(arguments["COMMAND"], env=env)
