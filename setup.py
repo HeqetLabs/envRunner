0a8b83c96227ef0e65dfb3329d453d62b961617b
Sean Reed <sean@cheapfit.me>
2015-03-20 20:57:31 -0400

    Fix the distribution and make sure that envRunner waits for the process to finish so we don't get zombies

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 from setuptools import setup
 
 setup(name='envRunner',
-    version='0.1.4',
+    version='0.1.6',
     description='Run a command with environment variables set from a url or file',
     author='Sean Reed',
     author_email='sean@reed.pub',
     url='https://github.com/HeqetLabs/envRunner',
     install_requires=['docopt', 'requests', 'PyYAML', 'wsgiref'],
-    setup_requires=['setuptools-markdown'],
     long_description_markdown_filename='README.md',
     scripts=['envRunner'],
     keywords=['environment', 'runner'],
