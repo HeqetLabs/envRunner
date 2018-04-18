0ad3e2ec3c3016ce9775eec9599c57c343166397
Sean Reed <reeds@valassis.com>
2018-04-18 16:32:57 -0700

    Some fixes to make this more compatiable with python3 as well as reupload to pypi.

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 from setuptools import setup
 
 setup(name='envRunner',
-    version='0.1.8',
+    version='0.2.0',
     description='Run a command with environment variables set from a url or file',
     author='Sean Reed',
     author_email='sean@reed.pub',
     url='https://github.com/HeqetLabs/envRunner',
-    install_requires=['docopt', 'requests', 'PyYAML', 'wsgiref'],
+    install_requires=['docopt', 'requests', 'PyYAML'],
     long_description_markdown_filename='README.md',
     scripts=['envRunner'],
     keywords=['environment', 'runner'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
 )
